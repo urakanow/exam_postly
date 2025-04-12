7881019ef032d42b2cbc168f654cd69632509ee8
Юрій Коноваленко <urakanow@gmail.com>
2025-04-12 17:21:53 +0200

    added connection string to appsettings development

--- exam_postly.Server/Program.cs
+++ exam_postly.Server/Program.cs
@@ -9,11 +9,11 @@ namespace exam_postly.Server
         public static void Main(string[] args)
         {
             //string connectionString = "Host=ep-yellow-dust-a2y1d5ag-pooler.eu-central-1.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_uzxe7qEfiTn1";
+            string connectionString = "Host=ep-yellow-dust-a2y1d5ag-pooler.eu-central-1.aws.neon.tech;Database=exam_postly_production;Username=prod_user;Password=npg_1eiWSCGu0afy";
+            string productionCors = "ProductionCorsPolicy";
             
             var builder = WebApplication.CreateBuilder(args);
 
-            string connectionString = "Host=ep-yellow-dust-a2y1d5ag-pooler.eu-central-1.aws.neon.tech;Database=exam_postly_production;Username=prod_user;Password=npg_1eiWSCGu0afy";
-            string productionCors = "ProductionCorsPolicy";
 
             builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseNpgsql(connectionString));
 
